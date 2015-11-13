1989318b4ed4b52ae0d8ca6fe0972e621d47d131
almustaness <e-comm@live.com>
2015-11-13 14:40:40 +0000

    partials for header and footer plus updated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   #in this line, we want to redirect the user to the controller:Pages and view:Home if he writes /home. Meaning we can create any path and redirect it to somewhere
   get '/home', to: 'pages#home'
+  #we need to make the main URL to be the home page by defaul. So we will write command
+  root 'pages#home'
 end
